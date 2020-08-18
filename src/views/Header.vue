b1d6b68178b726e6f823101d71c4cc729f4d9c44
Lanerxx <892907311@qq.com>
2020-08-18 20:22:04 +0800

    初始页面布局

---
+++ src/views/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app-bar
+    app
+    absolute
+    color="#43a047"
+    dark
+    shrink-on-scroll
+    prominent
+    src="https://picsum.photos/1920/1080?random"
+    fade-img-on-scroll
+    scroll-target="#scrolling-techniques-5"
+    scroll-threshold="500"
+    max-height="100px"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+      ></v-img>
+    </template>
+    <v-spacer></v-spacer>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          class="ma-2 font-italic font-weight-bold"
+          x-large
+          text
+          color="rgba(0, 128, 128, 0.712)"
+        >
+          大学生岗位匹配系统
+        </v-btn>
+      </template>
+      <span>主页</span>
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="ma-2"
+          outlined
+          fab
+          color="teal"
+          v-bind="attrs"
+          v-on="on"
+          to="/help"
+        >
+          <v-icon>mdi-format-list-bulleted-square</v-icon>
+        </v-btn>
+      </template>
+      <span>帮助</span>
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="ma-2"
+          outlined
+          fab
+          color="indigo"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+      </template>
+      <span>登出</span>
+    </v-tooltip>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  methods: {}
+};
+</script>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>sdas</h1>
+</template>
